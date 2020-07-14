1acd172a48760db6a7022f8d638adda54cc5b9f7
Adesiyan Oladipo <adesiyanoladipo1@gmail.com>
2020-07-13 20:00:40 -0700

    Update settings.py

--- mysite/settings.py
+++ mysite/settings.py
@@ -135,7 +135,7 @@ EMAIL_BACKEND = "django.core.mail.backends.smtp.EmailBackend"
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
-EMAIL_HOST_USER = 'labrielmedia@gmail.com'
+EMAIL_HOST_USER = 'messiahictteam@gmail.com'
 EMAIL_HOST_PASSWORD = 'Ogeneral2006'
 
 LOGIN_URL = 'login'
